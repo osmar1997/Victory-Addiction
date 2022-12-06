bc8eabbba1f53fb38f14f2296420c378240513ee
LAPTOP-TB89MLMH\silva <Fbio Lopes>
2022-12-06 10:23:53 +0000

    Porta da arena fecha apos jogador entrar

---
+++ Assets/AnimDoor/CloseDoor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoor : MonoBehaviour
+{
+    public GameObject AnimeObject;
+    public GameObject ThisTrigger;
+    public AudioSource DoorCloseSound;
+    public bool Action = false;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Action = false;
+    }
+
+
+    void Update()
+    {
+        
+      if (Action == true)
+      {
+           AnimeObject.GetComponent<Animator>().Play("CloseDoor");
+           ThisTrigger.SetActive(false);
+           DoorCloseSound.Play();
+           Action = false;
+      }
+    }
+}
