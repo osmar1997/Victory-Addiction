9ef2d0e31d3228ed804747ef87005528ca57fc2c
LAPTOP-TB89MLMH\silva <Fbio Lopes>
2023-01-08 14:18:06 +0000

    personagens secundarias novas em processo

--- Assets/Horse/HorseInteraction.cs
+++ Assets/Horse/HorseInteraction.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class HorseInteraction : MonoBehaviour
 {
+    public AudioSource itemSound;
+    public AudioSource soundWalk;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !other.GetComponent<PlayerController>().isMounted)
         {
             other.GetComponent<PlayerController>().SwitchToMount(true);
+            itemSound.Play();
+            //soundWalk.Play();
 
             gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
 
--- Assets/Scripts/PlayerInteract/Scripts/NpcInteratable.cs
+++ Assets/Scripts/PlayerInteract/Scripts/NpcInteratable.cs
@@ -7,17 +7,21 @@ public class NpcInteratable : MonoBehaviour, IInteractable
     [SerializeField] private string interactText;
 
     private Animator animator;
-   
+    private NPCHeadLookAt npcHeadLookAt;
+
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        npcHeadLookAt = GetComponent<NPCHeadLookAt>();
 
     }
 
     public void Interact(Transform interactorTransform)
     {
         ChatBubble.Create(transform.transform, new Vector3(-.3f, 1.7f, 0f), ChatBubble.IconType.Happy, GetInteractText());
+        float playerHeight = 1.7f;
+        npcHeadLookAt.LookAtPosition(interactorTransform.position + Vector3.up * playerHeight);
 
         //animator.SetTrigger("Talk");
 
